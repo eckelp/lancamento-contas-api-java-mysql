e17b68fe991cb27584e173b4d6c4e8ccb815b201
Gabriel Eckel Pelegrina <gabriel.pelegrina@outlook.com>
2021-01-29 09:41:53 -0300

    Iniciando o projeto

---
+++ src/main/java/br/eckelp/lancamentoconta/LancamentoContaApplication.java
@@ -0,0 +1,13 @@
+package br.eckelp.lancamentoconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentoContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentoContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/eckelp/lancamentoconta/LancamentoContaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eckelp.lancamentoconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentoContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
